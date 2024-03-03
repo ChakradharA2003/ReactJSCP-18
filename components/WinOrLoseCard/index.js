8203c7244ec3e0c922d7ea5b49a7582eb0aa0dc0
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-03-04 00:00:20 +0530

    Update index.js

--- components/WinOrLoseCard/index.js
+++ components/WinOrLoseCard/index.js
@@ -2,41 +2,69 @@
 import './index.css'
 
 const WinOrLoseCard = props => {
-  const {score} = props
+  const {score, onClickPlayAgain} = props
+  console.log(score)
+  const playAgain = () => {
+    onClickPlayAgain()
+  }
   if (score === 12) {
     return (
-      <div className="won-result-card">
-        <div className="result-details">
-          <h1 className="heading">You Won</h1>
-          <p className="para">Best Score</p>
-          <p className="scores">{score}/12</p>
-          <button type="button" className="play-btn">
-            Play Again
-          </button>
+      <div className="bg-container">
+        <div className="nav-container">
+          <div className="emoji-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+              alt="emoji logo"
+              className="nav-img"
+            />
+            <h1 className="emoji-heading">Emoji Game</h1>
+          </div>
         </div>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
-          alt="win or lose"
-          className="won-img"
-        />
+        <div className="won-result-card">
+          <div className="result-details">
+            <h1 className="heading">You Won</h1>
+            <p className="para">Best Score</p>
+            <p className="scores">{score}/12</p>
+            <button type="button" className="play-btn" onClick={playAgain}>
+              Play Again
+            </button>
+          </div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
+            alt="win or lose"
+            className="won-img"
+          />
+        </div>
       </div>
     )
   }
   return (
-    <div className="lose-result-card">
-      <div className="result-details">
-        <h1 className="heading">You Lose</h1>
-        <p className="para">Score</p>
-        <p className="scores">{score}/12</p>
-        <button type="button" className="play-btn">
-          Play Again
-        </button>
+    <div className="bg-container">
+      <div className="nav-container">
+        <div className="emoji-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+            className="nav-img"
+          />
+          <h1 className="emoji-heading">Emoji Game</h1>
+        </div>
       </div>
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
-        alt="win or lose"
-        className="won-img"
-      />
+      <div className="lose-result-card">
+        <div className="result-details">
+          <h1 className="heading">You Lose</h1>
+          <p className="para">Score</p>
+          <p className="scores">{score}/12</p>
+          <button type="button" className="play-btn" onClick={playAgain}>
+            Play Again
+          </button>
+        </div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/lose-game-img.png"
+          alt="win or lose"
+          className="won-img"
+        />
+      </div>
     </div>
   )
 }
